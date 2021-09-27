f0ffb378e40074702bf6024b03d81df8765e0f48
Nabeel Shakeel <k190795@nu.edu.pk>
2021-09-27 12:50:29 +0500

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hi There 5.0!!</h1>
-            <hr />
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
     );
 };
 
