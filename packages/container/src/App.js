7fe31cadbf80577ea087615cd9b3bc1eeb9bc69f
Nabeel Shakeel <k190795@nu.edu.pk>
2021-09-28 12:45:57 +0500

    Added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,33 @@
-import React, {lazy, Suspense, useState} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, {lazy, Suspense, useState, useEffect} from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider,  createGenerateClassName } from '@material-ui/core/styles';
+import { createBrowserHistory } from 'history';
+
 
 import Header from './components/Header';
 import Progress from './components/Progress';
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
 });
 
+const history = createBrowserHistory();
+
 const App = () => {
 
     const [isSignIn, setIsSignIn] = useState(false);
 
+    useEffect(() => {
+        if(isSignIn){
+            history.push('/dashboard');
+        }
+    }, [isSignIn]);
+
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header onSignOut={ ()=>{ setIsSignIn(false) }} isSignedIn={isSignIn}/>
@@ -25,12 +36,16 @@ const App = () => {
                             <Route path="/auth">
                                 <AuthLazy onSignIn={() => {setIsSignIn(true)}}/>
                             </Route>
+                            <Route path="/dashboard">
+                                { !isSignIn && <Redirect to="/" />}
+                                <DashboardLazy />
+                            </Route>
                             <Route path="/" component={MarketingLazy} />
                         </Switch>
                     </Suspense>
                 </div>
             </StylesProvider>
-        </BrowserRouter>
+        </Router>
     );
 };
 
---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard';
+
+// Mount funtiont to start up the app
+const mount = (el) => {
+    const app = createApp(Dashboard);
+    app.mount(el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_dashboard-dev-root');
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+export { mount };
